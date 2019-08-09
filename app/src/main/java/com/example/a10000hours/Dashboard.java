95577718a9a13782d3385acf6e59ad39433e7e34
Shafkhan <shafkhan@Shafkhans-MacBook-Pro.local>
2019-08-09 23:02:08 +0530

    Dashbaord first stats completed

---
+++ app/src/main/java/com/example/a10000hours/Dashboard.java
@@ -0,0 +1,16 @@
+package com.example.a10000hours;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+    }
+}
