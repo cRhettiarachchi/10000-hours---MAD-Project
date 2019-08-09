8943e5e2a8cfc0c4cac731672b6ee1c82c66866c
khan9920 <shafkhanmohamed23@gmail.com>
2019-08-09 23:54:53 +0530

    Dashboard completed

--- app/src/main/java/com/example/a10000hours/Dashboard.java
+++ app/src/main/java/com/example/a10000hours/Dashboard.java
@@ -3,16 +3,37 @@ package com.example.a10000hours;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Dashboard extends AppCompatActivity {
+
+    private Button buttonAddGoal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        buttonAddGoal = findViewById(R.id.button2);
+
+        buttonAddGoal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addGoal();
+            }
+        });
+
     }
+
+    public void addGoal() {
+        Intent intent = new Intent(this, AddGoal.class);
+        startActivity(intent);
+    }
+
+
 }
