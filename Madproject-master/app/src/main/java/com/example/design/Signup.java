a75da7f90243f179da8dbb1b58c58ba66cb0844f
sasika9511 <sashi951109@gmail.com>
2019-08-09 17:17:31 +0530

    update string

---
+++ Madproject-master/app/src/main/java/com/example/design/Signup.java
@@ -0,0 +1,34 @@
+package com.example.design;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Signup extends AppCompatActivity {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singup);
+
+        Toolbar toolbar = findViewById(R.id.historyToolbar);
+        setSupportActionBar(toolbar);
+        setTitle("Sign up");
+
+        button = (Button) findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userProfile();
+            }
+        });
+    }
+    public void  userProfile(){
+        Intent intent = new Intent(this,Userprofile.class);
+        startActivity(intent);
+    }
+}
