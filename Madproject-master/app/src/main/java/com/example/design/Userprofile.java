24648949cf68dcaf59cc76f68474fd8d818efba0
Charith.r <charith.rhettiarachchi@gmail.com>
2019-08-09 16:53:21 +0530

    updated

---
+++ Madproject-master/app/src/main/java/com/example/design/Userprofile.java
@@ -0,0 +1,16 @@
+package com.example.design;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class Userprofile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userprofile);
+
+    }
+}
